9f950f04614f426a71ad74671defd623dbc501d0
mattb61 <146943736+mattb61@users.noreply.github.com>
2024-12-05 19:53:21 -0330

    THE QUESTION SWAPPING ON BUTTON CLICK FINALLY WORKS

--- CourseProject.js
+++ CourseProject.js
@@ -1,16 +1,178 @@
+// If statement to check each question title. Nested If statement to check button clicked.
+
 "use strict";
 
 const $ = selector => document.querySelector(selector);
 
-const scoreKeeper = () => {
-    let score = 0;
+let answer1 = document.getElementById("start");
+let answer2 = document.getElementById("answer2");
+let answer3 = document.getElementById("answer3");
+let answer4 = document.getElementById("answer4");
+let qTitle = document.getElementById("questionTitle");
+
+const checkAnswer = () => {
+    if (qTitle.textContent == "Who was the first Elden Lord?") {
+        answer2.addEventListener("click", () => {
+            secondQuestion();
+            sessionStorage.setItem("score", "1");
+        });
+    }
+    
+    if (qTitle.textContent == "Who was responsible for the Night of the Black Knives?") {
+        answer3.addEventListener("click", () => {
+            thirdQuestion();
+            sessionStorage.setItem("score", "2");
+        });
+    }
+
+    if (qTitle.textContent == "Who resurrected and recalled the Tarnished to become Elden Lord?") {
+        answer3.addEventListener("click", () => {
+            fourthQuestion();
+            sessionStorage.setItem("score", "3");
+        });
+    }
+
+    if (qTitle.textContent == "What is the other name for the Rune of Death?") {
+        answer4.addEventListener("click", () => {
+            fifthQuestion();
+            sessionStorage.setItem("score", "4");
+        });
+    }
+
+    if (qTitle.textContent == "Who is the strongest demigod in terms of abilities?") {
+        answer2.addEventListener("click", () => {
+            sixthQuestion();
+            sessionStorage.setItem("score", "5");
+        });
+    }
+
+    if (qTitle.textContent == "Who is the final boss of Elden Ring?") {
+        answer3.addEventListener("click", () => {
+            seventhQuestion();
+            sessionStorage.setItem("score", "6");
+        });
+    }
+
+    if (qTitle.textContent == "Which dragon's spear is located in Leyndell? (Literal and/or item pickup)") {
+        answer4.addEventListener("click", () => {
+            eighthQuestion();
+            sessionStorage.setItem("score", "7");
+        });
+    }
+
+    if (qTitle.textContent == "Who is the first major boss you're TECHNICALLY supposed to fight?") {
+        answer2.addEventListener("click", () => {
+            ninthQuestion();
+            sessionStorage.setItem("score", "8");
+        });
+    }
+
+    if (qTitle.textContent == "What is the name of the tree Miquella planted to replace the Erdtree?") {
+        answer4.addEventListener("click", () => {
+            tenthQuestion();
+            sessionStorage.setItem("score", "9");
+        });
+    }
+
+    if (qTitle.textContent == "Who is St. Trina?") {
+        answer3.addEventListener("click", () => {
+            sessionStorage.setItem("score", "10");
+        })
+    }
 }
 
-const pageSwap = () => {
-    self.location = ("FirstQuestion.html");
+const firstQuestion = () => {
+    qTitle.textContent = "Who was the first Elden Lord?";
+    qTitle.setAttribute("class", "visible");
+    answer1.setAttribute("class", "visible");
+    answer1.textContent = "Hoarah Loux";
+    answer2.setAttribute("class", "visible");
+    answer2.textContent = "Dragonlord Placidusax";
+    answer3.setAttribute("class", "visible");
+    answer3.textContent = "Radagon";
+    answer4.setAttribute("class", "visible");
+    answer4.textContent = "The Tarnished";
+    sessionStorage.setItem("score", "0");
+}
+
+const secondQuestion = () => {
+    qTitle.textContent = "Who was responsible for the Night of the Black Knives?";
+    answer1.textContent = "Godwyn";
+    answer2.textContent = "Queen Marika";
+    answer3.textContent = "Ranni the Witch";
+    answer4.textContent = "You? Somehow?";
+}
+
+const thirdQuestion = () => {
+    qTitle.textContent = "Who resurrected and recalled the Tarnished to become Elden Lord?";
+    answer1.textContent = "Elden Lord Godfrey";
+    answer2.textContent = "Gideon Ofnir";
+    answer3.textContent = "Queen Marika";
+    answer4.textContent = "Renna the Witch";
+}
+
+const fourthQuestion = () => {
+    qTitle.textContent = "What is the other name for the Rune of Death?";
+    answer1.textContent = "Absolute Death";
+    answer2.textContent = "The Rune of the End";
+    answer3.textContent = "The Judgement Rune";
+    answer4.textContent = "Destined Death";
+}
+
+const fifthQuestion = () => {
+    qTitle.textContent = "Who is the strongest demigod in terms of abilities?";
+    answer1.textContent = "Malenia, Blade of Miquella";
+    answer2.textContent = "Miquella the Kind";
+    answer3.textContent = "Ranni the Witch";
+    answer4.textContent = "Starscourge Radahn";
+}
+
+const sixthQuestion = () => {
+    qTitle.textContent = "Who is the final boss of Elden Ring?";
+    answer1.textContent = "Radagon";
+    answer2.textContent = "Malenia";
+    answer3.textContent = "The Elden Beast";
+    answer4.textContent = "Queen Marika";
+}
+
+const seventhQuestion = () => {
+    qTitle.textContent = "Which dragon's spear is located in Leyndell? (Literal and/or item pickup)";
+    answer1.textContent = "Sennesax";
+    answer2.textContent = "Fortissax";
+    answer3.textContent = "Lansseax";
+    answer4.textContent = "Gransax";
+}
+
+const eighthQuestion = () => {
+    qTitle.textContent = "Who is the first major boss you're TECHNICALLY supposed to fight?";
+    answer1.textContent = "Queen Rennala";
+    answer2.textContent = "Margit, the Fell Omen";
+    answer3.textContent = "Morgot, the Fell Omen";
+    answer4.textContent = "Godrick";
+}
+
+const ninthQuestion = () => {
+    qTitle.textContent = "What is the name of the tree Miquella planted to replace the Erdtree?";
+    answer1.textContent = "The Erdtree (seriously?)";
+    answer2.textContent = "An oak tree";
+    answer3.textContent = "The World Tree";
+    answer4.textContent = "The Haligtree";
+}
+
+const tenthQuestion = () => {
+    qTitle.textContent = "Who is St. Trina?";
+    answer1.textContent = "Some random priest";
+    answer2.textContent = "Some random saint";
+    answer3.textContent = "Miqeulla's alter ego";
+    answer4.textContent = "Miquella's old friend";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    $("#start").addEventListener("click", pageSwap);
-    scoreKeeper();
+    $("#start").addEventListener("click", firstQuestion);
+
+    let timer = null;
+
+    if(timer == null) {
+        timer = setInterval(checkAnswer, 90);
+    }
 });
